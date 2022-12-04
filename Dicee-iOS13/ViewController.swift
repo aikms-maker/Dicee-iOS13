d94f6eb14d9dbdd04c37790ee7c22afa2a344fd8
aikms-maker <aikms0011@outlook.jp>
2022-12-04 23:26:20 +0900

    リファクタリング

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -15,15 +15,15 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var rollButton: UIButton!
     
-    var leftDiceNumber = 0
-    var rightDiceNumber = 5
-    
-    let limitNumberOfLeft = 5
-    let limitNumberOfRight = 0
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+//    var leftDiceNumber = 0
+//    var rightDiceNumber = 5
+//
+//    let limitNumberOfLeft = 5
+//    let limitNumberOfRight = 0
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
 //        // 左ダイス６の目設定
 //        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
 //        // 左ダイス透明度設定
@@ -32,13 +32,13 @@ class ViewController: UIViewController {
 //        // 右ダイス２の目設定
 //        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
 //        // 右ダイス透明度設定
-//        diceImageView2.alpha = 0.8
-    }
+//       diceImageView2.alpha = 0.8
+//    }
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
 //        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceFour")
 //        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceFour")
-        var diceNumbers = [
+        let diceNumbers = [
             UIImage(imageLiteralResourceName: "DiceOne"),
             UIImage(imageLiteralResourceName: "DiceTwo"),
             UIImage(imageLiteralResourceName: "DiceThree"),
@@ -49,21 +49,24 @@ class ViewController: UIViewController {
 
         // 左　インクリメント　右　デクリメント
         // 初期値
-        diceImageView1.image = diceNumbers[leftDiceNumber]
-        diceImageView2.image = diceNumbers[rightDiceNumber]
-
-        leftDiceNumber = leftDiceNumber + 1
-        rightDiceNumber = rightDiceNumber - 1
-        
-        if(leftDiceNumber > limitNumberOfLeft || rightDiceNumber < limitNumberOfRight) {
-            rollButton.isEnabled = false
-        }
+//        diceImageView1.image = diceNumbers[leftDiceNumber]
+//        diceImageView2.image = diceNumbers[rightDiceNumber]
+//
+//        leftDiceNumber += 1
+//        rightDiceNumber -= 1
+//
+//        if(leftDiceNumber > limitNumberOfLeft || rightDiceNumber < limitNumberOfRight) {
+//            rollButton.isEnabled = false
+//        }
         
         
         
 //        // ランダム
 //
-//        diceImageView1.image = diceValues.randomElement()
-//        diceImageView2.image = diceValues.randomElement()
+        diceImageView1.image = diceNumbers.randomElement()
+        diceImageView2.image = diceNumbers.randomElement()
+        
+//        diceImageView1.image = diceNumbers[Int.random(in: 0...5)]
+//        diceImageView2.image = diceNumbers[Int.random(in: 0...5)]
     }
 }
